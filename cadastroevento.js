237352b5fb32607d86ac729758cd941162d00d7b
Victor <victorlangnier2@gmail.com>
2021-10-14 18:23:05 -0300

    Sistema de cadastro alterado

--- cadastroevento.js
+++ cadastroevento.js
@@ -9,12 +9,12 @@ if (dia < 4 && mes <= 11 && ano <= 2021)
     console.log("Você ainda pode se cadastrar!")
     
     for (listaTotalPessoas = 0; listaTotalPessoas < 15; listaTotalPessoas++)
- {
-    console.log("Participante número " + listaTotalPessoas)
-
-    if (listaTotalPessoas >= 14)
     {
-        console.log("Lista ainda possui espaço!")
+        console.log("Participante número " + listaTotalPessoas)
+
+        if (listaTotalPessoas >= 14)
+        {
+            console.log("Lista ainda possui espaço!")
         
             //Aqui suponhamos que o usuário digitou X idade e o valor foi enviado para a variável "idadeUser" 
 
@@ -30,12 +30,12 @@ if (dia < 4 && mes <= 11 && ano <= 2021)
             {
                 console.log("Cadastro negado: idade " + (idadeUser) + " é menor (ou igual) a 18.")
             }
+        }
+      else if (listaTotalPessoas >= 99)
+      {
+          console.log("Acesso negado: limite da lista alcançada.")
+      }
     }
-    else if (listaTotalPessoas >= 99)
-    {
-        console.log("Acesso negado: limite da lista alcançada.")
-    }
- }
 }
 else
 {
